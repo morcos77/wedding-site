55054fc864cbc84729f7d8472a3861283e6b775c
morcos77 <85204495+morcos77@users.noreply.github.com>
2026-05-24 17:25:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const weddingDate = new Date("Feb 12, 2027 11:00:00").getTime();
+
+setInterval(function () {
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + " يوم | " + hours + " ساعة | " + minutes + " دقيقة | " + seconds + " ثانية";
+
+  if (distance < 0) {
+    document.getElementById("countdown").innerHTML = "💍 اليوم الكبير وصل 💍";
+  }
+}, 1000);
