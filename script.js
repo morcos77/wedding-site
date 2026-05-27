5fe2233d66b7458047d89ba036ead9f542adacfd
morcos77 <85204495+morcos77@users.noreply.github.com>
2026-05-27 08:16:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const weddingDate = new Date("Feb 27, 2027 11:00:00").getTime();
 setInterval(() => {
   const now = new Date().getTime();
   const distance = weddingDate - now;
-
   const d = Math.floor(distance / (1000*60*60*24));
   const h = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
   const m = Math.floor((distance%(1000*60*60))/(1000*60));
   const s = Math.floor((distance%(1000*60))/1000);
 
   const countdownEl = document.getElementById("countdown");
-  if(countdownEl) {
-      countdownEl.innerHTML = `${d} Days | ${h}h | ${m}m | ${s}s`;
-  }
+  if(countdownEl) countdownEl.innerHTML = `${d} Days | ${h}h | ${m}m | ${s}s`;
 }, 1000);
 
 function openQR() { document.getElementById("qr-popup").style.display = "flex"; }
